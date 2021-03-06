f442c3ad1e58ec1f7798cf618a271d539151bbcb
evgenii20 <zev_ps@mail.ru>
2021-03-06 08:29:09 +0500

    lesson_2_hw

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,35 @@
+from datetime import datetime
+
 from django.shortcuts import render
+
 
 # Create your views here.
 # Принимаем запросы от пользователей "request" и возвращаем "request" чтоб не потерять цепочку запросов
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'Главная',
+    }
+    return render(request, 'mainapp/index.html', content)
+
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    # описание url
+    links_menu = [
+        {'href': 'products_all', 'name': 'все'},
+        {'href': 'products_home', 'name': 'дом'},
+        {'href': 'products_office', 'name': 'офис'},
+        {'href': 'products_modern', 'name': 'модерн'},
+        {'href': 'products_klassic', 'name': 'классика'},
+    ]
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
+
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', content)
