db53ee21953ede36b688e582aec004a7582dc764
evgenii20 <zev_ps@mail.ru>
2021-04-26 20:53:07 +0500

    lesson_14_6

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -5,7 +5,10 @@ def basket(request):
     print(f'context processor basket works')
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user)
+        # basket_list = Basket.objects.filter(user=request.user)
+        # basket_list = Basket.objects.filter(user=request.user).order_by('product__category')
+        # basket = Basket.get_item(request.user)
+        basket_list = Basket.objects.filter(user=request.user).select_related()
     return {
         'basket': basket_list
     }
